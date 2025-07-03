80aee581819fc40fe59316db432b1522a62875f6
Sandro Huaylla <a20171364@pucp.edu.pe>
2025-07-03 17:57:38 -0500

    healthController

---
+++ src/main/java/pe/com/tiendaPerfumes/Controller/HealthController.java
@@ -0,0 +1,18 @@
+package pe.com.tiendaPerfumes.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
+
+
+
+
+
